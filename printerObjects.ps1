e1050a2a36f45a1ed53a482327fe4fb696098e29
Loyd Padgett <loydpadgett@gmail.com>
2021-05-16 18:24:32 -0400

    Added class Action() for better control
    Signed-off-by: Loyd Padgett <loydpadgett@gmail.com>

--- printerObjects.ps1
+++ printerObjects.ps1
@@ -1,19 +1,32 @@
-class Snapshot {
-    #snapshot properties
-    [string]$Server;
-    [string]$Printer;
-    [int]$Action
-    [string]$Flag
-    #use class for variable validation and error control 
-    Snapshot(
+class Printer {
+    #printer properties
+    [string]$Server
+    [string]$Printer
+    #use method for variable validation and error control 
+    Printer(
     [string]$s,
-    [string]$p,
-    [int]$a,
-    [string]$f
-    ){
+    [string]$p
+        ){
     $this.Server = $s
     $this.Printer = $p
-    $this.Action = $a
-    $this.Flag = $f
+    }
+}
+class Action {
+    #action properties
+    #use class for variable validation to determine what needs to be done
+    [int]$Exec
+    [string]$Flag
+    Action(
+        [int]$E = 0..2,
+        [string]$F
+    ){
+    if($this.Exec -ge 3)
+    {
+      #this should limit error 
+    } elsef ($this.Exec -le 2)
+    {
+        $this.Exec = $E
+    }
+    $this.Flag = $F
     }
 }
