77183ba90c14f7b3d9438cf8810113ad0e3d1fc7
Loyd Padgett <loydpadgett@gmail.com>
2021-05-16 10:12:04 -0400

    Update printerObjects.ps1
    Changed properties to match param(){} values in main loop

--- printerObjects.ps1
+++ printerObjects.ps1
@@ -2,7 +2,9 @@ class Snapshot {
     #snapshot properties
     [string]$Server;
     [string]$Printer;
-
+    [int]$action
+    [string]$flag
+    #use class for variable validation and error control 
     Snapshot(
     [string]$s,
     [string]$p,
