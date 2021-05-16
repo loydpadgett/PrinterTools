e820db9b515c9d6d83bb08012cf11037b0638087
Loyd Padgett <loydpadgett@gmail.com>
2021-05-16 10:08:51 -0400

    Update and rename pirinterObjects.ps1 to printerObjects.ps1
    fixed misspell of printerObjects.ps1 and removed old variables

--- printerObjects.ps1
+++ printerObjects.ps1
@@ -2,14 +2,11 @@ class Snapshot {
     #snapshot properties
     [string]$Server;
     [string]$Printer;
-    [string]$
 
     Snapshot(
     [string]$s,
     [string]$p,
-    [string]$vf
     ){
     $this.Server = $s
     $this.Vm = $v
-    $this.Vmfolder = $vf
     }
