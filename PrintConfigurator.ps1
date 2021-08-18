df1f98e66a9430627a39f8a99fce7cbe63dbb4cb
Loyd Padgett <loydpadgett@gmail.com>
2021-08-17 21:36:53 -0400

    Added a local printer PRINT function and a network printer PRINT function, also cleaned up printerObjects, brainstorming on how to handle the leftover  variable

--- PrintConfigurator.ps1
+++ PrintConfigurator.ps1
@@ -15,17 +15,32 @@
 param(
         [string]$SERVER,       
         [string]$printer,
-        [string]$flag = 'list'
+        [string]$flag = 'list',
+        [string]$action = 'local'
 )
 . .\printerObjects.ps1
 #create objects and apply attributes
-$uAct = [UserAction]::new($flag)
+$uAct = [UserAction]::new($flag,$action)
 $uPrinter = [Printer]::new($Server,$printer)    
 $PrinterFormatted = $uPrinter.Printer.ToUpper()
 function DisplayPrinters{
-    $printerlist=Get-Printer
-    $printerlist | Where-Object {$_.Type -notlike "Local"} | `
-    Select-Object -Property Name, ComputerName, DriverName
+    #this pipeline moves over multiple lines, fyi
+       
+    function LocalPrinter {
+        $printerlist = Get-Printer | Where-Object {$_.Name -ilike "*$Printer*"}Where-Object {$_.Type -notlike "Local"} | `
+        Select-Object -Property Name, ComputerName, DriverName
+        Write-Output $printerlist
+    }
+    function NetworkPrinter {
+        $netprinterlist = Get-Printer -ComputerName "print01.ua.lan" | Where-Object {$_.Name -notlike "*Microsoft*"} | `
+        Select-Object -Property Name, ComputerName, DriverName
+        Write-Output $netprinterlist
+    }
+   switch ($uAct.Action) {
+        local { LocalPrinter }
+        network { NetworkPrinter }
+        Default { LocalPrinter }
+    }  
 }
     #perform specific action when 'display' keyword is used
 function AddPrinter{
@@ -33,7 +48,7 @@ function AddPrinter{
     [bool]$printerInstalled = $false
     #use a loop to verify the printer is either already installed/skip or 
     do {
-        if(Get-Printer | Where-Object {$_.Name -ilike "*$printer*"}){
+        if(Get-Printer | Where-Object {$_.Name -ilike "*$printer*"} -and Where-Object{$_.Name -notlike "Local"}){
             Write-Output "*******$uPrinter.printer has been successfully installed*******" -NoEnumerate
             $printerInstalled = $true
             Break
