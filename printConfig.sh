af9f4ce521ae527354326813fece186562e1ed7a
Loyd Padgett <loydpadgett@gmail.com>
2021-11-01 14:54:10 -0400

    Updated most values in script to be current.
    Sanitized values for public usage.  Began editing some parts of the file.  TODO convert `if()` statement into a recursive `switch()` statement using provided options from the command line.

--- printConfig.sh
+++ printConfig.sh
@@ -1,4 +1,40 @@
-#!/bin/bash
-##this is a place holder for while loop
-##I am going to go to the office to try and dig this awk program I wrote years ago. 
-while read;
+#!/usr/bin/awk -f
+## this is an old awk command I wrote to dynamically add printers from flags
+## I will use this to work from the other script.
+BEGIN {
+	   for (i = 1; i < ARGC; i++){
+      
+		  if (ARGV[i] "\-^[aAtTmMhHsS]"){
+        #Regular expression to better filter options
+			  printer = ""
+			  printerqueue = ""
+			  driver = "5945_PS.ppd"
+			}	   		
+	   	if (ARGV[i] == "-"){
+			  printer = ""
+			  printerqueue = ""
+			  driver = "5945_PS.ppd"
+	   	}
+	   	if (ARGV[i] == "-"){
+			  printer = ""
+			  printerqueue = ""
+			  driver = "5945_PS.ppd"
+	   	}
+	   	if (ARGV[i] == "-"){
+			  printer = ""
+			  printerqueue = ""
+			  driver = "5945_PS.ppd"
+	   	}
+	   	if (ARGV[i] == "-"){
+			  printer = ""
+			  printerqueue = ""
+			  driver = "5945_PS.ppd"
+	   	}
+	   		{
+				  system("lpadmin -p "printer" -v lpd:///"printerqueue" -E -P "driver")	
+				}
+	   	}
+	}
+	
+	
+
